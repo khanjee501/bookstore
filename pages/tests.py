dde54f9813ba16cdeef4a80e41c63d59906a3581
Saro <57326765+khanjee501@users.noreply.github.com>
2025-04-08 18:44:25 +0500

    Refactor: Adding setUp method on top which will help DRY code

--- pages/tests.py
+++ pages/tests.py
@@ -4,22 +4,18 @@ from django.urls import reverse
 
 
 class HomePageTests(SimpleTestCase):
-    def test_url_exists_at_correct_location(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
 
-    def test_homepage_url_name(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(self.response, "home.html")
 
     def test_homepage_contains_correct_html(self):
-        response = self.client.get("/")
-        self.assertContains(response, "Home Page")
+        self.assertContains(self.response, "Home Page")
 
     def test_homepage_does_not_contain_incorrect_html(self):
-        response = self.client.get("/")
-        self.assertNotContains(response, "Hi there, I should not be there.")
+        self.assertNotContains(self.response, "Hi there, I should not be there.")
