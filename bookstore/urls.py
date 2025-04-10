fd527cbaef5c1e23fa78d1d238d24df40bd386a8
Saro <57326765+khanjee501@users.noreply.github.com>
2025-04-10 13:53:55 +0500

    Feat: Signup form and functionality added

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     # User management
     path("accounts/", include("django.contrib.auth.urls")),
     # Local Apps
+    path("accounts/", include("accounts.urls")),
     path("", include("pages.urls")),
 ]
