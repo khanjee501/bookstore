6c7d5b8993330048315720fe481ed127f4d06282
Saro <57326765+khanjee501@users.noreply.github.com>
2025-04-10 18:09:58 +0500

    Code: static directory and its sub dirs and files added and configured settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -70,9 +70,6 @@ TEMPLATES = [
     },
 ]
 
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
-
 WSGI_APPLICATION = "bookstore.wsgi.application"
 
 
@@ -126,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
