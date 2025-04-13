e935cdd763b4e60662a477b83833ec01c2d1984a
Saro <57326765+khanjee501@users.noreply.github.com>
2025-04-13 14:41:35 +0500

    Style: Styled forms using BOOTSTRAP

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-eh*@r_u7hy$&fk+bpj$q8vj0zcxko=@=c^-7@s6xrq)i9a7o65"
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
