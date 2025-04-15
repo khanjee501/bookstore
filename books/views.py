4f0d6f939b044e091ab77ec3fa8e7da7b801af3a
Saro <57326765+khanjee501@users.noreply.github.com>
2025-04-15 16:58:02 +0500

    Using Q to filter through multiple conditions

--- books/views.py
+++ books/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 from django.views.generic import ListView, DetailView
 
@@ -17,3 +18,14 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     template_name = "books/book_detail.html"
     login_url = "account_login"
     permission_required = "books.special_status"
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = "books/search_results.html"
+
+    def get_queryset(self):  # new
+        return Book.objects.filter(
+            Q(title__icontains="beginners") | Q(title__icontains="api")
+        )
